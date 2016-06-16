f9c40c7caa1afbb55ba1aa111aab077363932115
egervan <ivant66@yandex.ru>
2016-06-16 16:52:51 +0300

    Реализовал корректную фильтрацию. Теперь фильтруется отдельно по дате и отдельно по времени.

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -12,8 +12,14 @@ public class TimeUtil {
     public static final DateTimeFormatter DATE_TME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static boolean isBetween(LocalDateTime lt, LocalDateTime startTime, LocalDateTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
+        return lt.toLocalTime().compareTo(startTime.toLocalTime()) >= 0 && lt.toLocalTime().compareTo(endTime.toLocalTime()) <= 0
+               && lt.toLocalDate().compareTo(startTime.toLocalDate()) >= 0 && lt.toLocalDate().compareTo(endTime.toLocalDate()) <= 0;
     }
+
+    /* default method
+    public static boolean isBetween(LocalDateTime lt, LocalDateTime startTime, LocalDateTime endTime) {
+        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0 ;
+    }*/
 
     public static String toString(LocalDateTime ldt) {
         return ldt == null ? "" : ldt.format(DATE_TME_FORMATTER);
