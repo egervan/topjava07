75df781a41ee45c22195a6999d8598a2b812f777
Ivan Teleshov <ivant66@yandex.ru>
2016-07-05 21:14:53 +0300

    Тесты проходят без ошибок. Дело было в том, что я не правильно реализовал методы в ProxyMealRepository и из-за этого Spring не мог заинжектить зависимости и выдавал failed Load ApplicationContest

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
@@ -13,28 +14,37 @@ import java.util.List;
  */
 @Repository
 public class DataJpaUserMealRepositoryImpl implements UserMealRepository{
+
+
+    @Autowired
+    private ProxyMealRepository proxy;
+
+    @Autowired
+    private ProxyUserRepository userProxy;
+
     @Override
     public UserMeal save(UserMeal userMeal, int userId) {
-        return null;
+        userMeal.setUser(userProxy.findOne(userId));
+        return proxy.save(userMeal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return proxy.delete(id, userId) != 0;
     }
 
     @Override
     public UserMeal get(int id, int userId) {
-        return null;
+        return proxy.findOne(id);
     }
 
     @Override
     public List<UserMeal> getAll(int userId) {
-        return null;
+        return proxy.findAll();
     }
 
     @Override
     public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return proxy.findAllByUserIdAndDateTimeBetweenOrderByDateTimeDesc(startDate, endDate, userId);
     }
 }
---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/ProxyMealRepository.java
@@ -0,0 +1,39 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.UserMeal;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Created by Jager on 05.07.2016.
+ */
+
+@Transactional(readOnly = true)
+public interface ProxyMealRepository extends JpaRepository<UserMeal, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM UserMeal m WHERE m.id=:id AND m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Override
+    @Transactional
+    UserMeal save(UserMeal userMeal);
+/*
+
+    @Modifying
+    UserMeal findOne(Integer id, Integer userId);
+
+    @Modifying
+    @Query("SELECT FROM UserMeal m WHERE m.user.id=:userId")
+    List<UserMeal> findAll(@Param("userId") int userId);
+*/
+
+    List<UserMeal> findAllByUserIdAndDateTimeBetweenOrderByDateTimeDesc(LocalDateTime startDate, LocalDateTime endDate, int userId);
+}
