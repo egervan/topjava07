f6d67d4f3ec3e778fed5f269f6905daf8c9eb032
egervan <ivant66@yandex.ru>
2016-07-06 17:28:01 +0300

    Добавил UserMealServiceJDBCTest

---
+++ src/test/java/ru/javawebinar/topjava/service/abstractTest/UserMealServiceJDBCTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service.abstractTest;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+
+/**
+ * Created by nz on 06.07.16.
+ */
+@ActiveProfiles(profiles = {Profiles.ACTIVE_DB, Profiles.JDBC})
+public class UserMealServiceJDBCTest extends UserMealServiceTest {
+}
