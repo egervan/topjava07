1f6373150da28e03e2e4b71f0cf678d3f0553cfe
Ivan Teleshov <ivant66@yandex.ru>
2016-07-08 23:08:41 +0300

    HW6 - убрал синтаксическую ошибку - был пробел в импорте

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,7 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.suppo   rt.WebApplicationContextUtils;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.util.TimeUtil;
 import ru.javawebinar.topjava.web.meal.UserMealRestController;
