f73efd3375f6d04342be6eb29fec34ed85191243
egervan <ivant66@yandex.ru>
2016-07-19 11:14:59 +0300

    Добавил тест RootControllerTest.testMealList для mealList.jsp. Тест проходит успешно

--- src/test/java/ru/javawebinar/topjava/web/user/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/RootControllerTest.java
@@ -7,8 +7,10 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.MEAL1;
+import static ru.javawebinar.topjava.MealTestData.MEAL1_ID;
 import static ru.javawebinar.topjava.UserTestData.USER;
-import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 /**
  * GKislin
@@ -26,8 +28,26 @@ public class RootControllerTest extends AbstractControllerTest {
                 .andExpect(model().attribute("userList", hasSize(2)))
                 .andExpect(model().attribute("userList", hasItem(
                         allOf(
-                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("id", is(USER_ID)),
                                 hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+
+    @Test
+    public void testMealList() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("mealList"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/mealList.jsp"))
+                .andExpect(model().attribute("mealList", hasSize(6)))
+                .andExpect(model().attribute("mealList", hasItem(
+                        allOf(
+                                hasProperty("id", is(MEAL1_ID)),
+                                hasProperty("description", is(MEAL1.getDescription())),
+                                hasProperty("calories", is(MEAL1.getCalories())),
+                                hasProperty("dateTime", is(MEAL1.getDateTime()))
                         )
                 )));
     }
