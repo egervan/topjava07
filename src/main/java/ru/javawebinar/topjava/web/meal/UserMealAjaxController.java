8f8456c030c5ac5b21f497ddad2f27622166fd10
egervan <ivant66@yandex.ru>
2016-07-25 17:23:48 +0300

    В классе UserMealAjaxController заменил методы update и create на метод createOrUpdate.

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealAjaxController.java
@@ -1,10 +1,12 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.to.UserMealWithExceed;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -26,16 +28,24 @@ public class UserMealAjaxController extends AbstractUserMealController {
         return super.get(id);
     }
 
-    @RequestMapping(value="{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value="{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable("id") Integer id)
     {
         super.delete(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public UserMeal create(@RequestParam("meal") UserMeal meal)
-    {
-        return super.create(meal);
+    @RequestMapping(method = RequestMethod.POST)
+    public void createOrUpdate(@RequestParam("id") Integer id,
+                           @RequestParam("description") String description,
+                           @RequestParam("calories") Integer calories,
+                           @RequestParam("dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime) {
+        UserMeal meal = new UserMeal(id, dateTime, description, calories);
+        if(meal.isNew()) {
+            super.create(meal);
+        }
+        else {
+            super.update(meal, meal.getId());
+        }
     }
 
     @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
